b702d39c5e77f76493a5cf6f88db51ca7af99469
Michael Hinte <mhinte91@gmail.com>
2019-11-06 12:35:06 -0600

    creates Character model

---
+++ models/Character.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const StatsSchema = mongoose.Schema({
+  strength: { type: Number, required: true },
+  dexterity: { type: Number, required: true },
+  constitution: { type: Number, required: true },
+  intelligence: { type: Number, required: true },
+  wisdom: { type: Number, required: true },
+  charisma: { type: Number, required: true },
+  hitpoints: { type: Number },
+  experience: { type: Number }
+});
+
+const CharacterSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  race: {
+    type: String,
+    required: true
+  },
+  class: {
+    type: String,
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  stats: [StatsSchema]
+});
+
+module.exports = mongoose.model('character', CharacterSchema);
