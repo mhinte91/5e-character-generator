ad2da0dd8f4a144a009f46275e9cc31ab76c5ad0
Michael Hinte <mhinte91@gmail.com>
2019-11-06 11:23:51 -0600

    registers user to database and hashes password

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 
@@ -9,6 +10,7 @@ const User = require('../models/User');
 // @access      Public
 router.post(
   '/',
+  // Check to see that fields are appropriately filled
   [
     check('name', 'Please enter your name')
       .not()
@@ -21,13 +23,42 @@ router.post(
       min: 6
     })
   ],
-  (req, res) => {
+  // Asynchronous function that first checks for the error validation,
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    // Destructures name, email, and password from the request object
+    const { name, email, password } = req.body;
 
-    res.send('passed');
+    try {
+      // If no errors, tries to find the user by the provided email
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      // If no user is found, a new User model is created with name, email, and password
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      // Hashes the password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Saves a new user to the database
+      await user.save();
+
+      res.send('User saved');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
