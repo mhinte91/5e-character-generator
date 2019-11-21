4cf8afced30a68834a0414f3bfb69a2ffb23d969
Michael Hinte <mhinte91@gmail.com>
2019-11-21 12:35:25 -0600

    creates Navbar and router setup

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About This App</h1>
+      <p className='my-1'>This is DnD character creation app </p>
+      <p className='bg-dark p'>Version 1.0.1</p>
+    </div>
+  );
+};
+
+export default About;
