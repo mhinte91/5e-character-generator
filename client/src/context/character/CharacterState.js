fe7b403b3b29b703f6f082ce900b6c44eb4b8d99
Michael Hinte <mhinte91@gmail.com>
2019-11-21 12:56:06 -0600

    creates character context

---
+++ client/src/context/character/CharacterState.js
@@ -0,0 +1,86 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import CharacterContext from './characterContext';
+import characterReducer from './characterReducer';
+import {
+  ADD_CHARACTER,
+  DELETE_CHARACTER,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CHARACTER,
+  FILTER_CHARACTERS,
+  CLEAR_FILTER
+} from '../types';
+
+const CharacterState = props => {
+  const initialState = {
+    characters: [
+      {
+        id: 1,
+        name: 'Merik',
+        race: 'Half-Orc',
+        heroClass: 'Fighter',
+        bio: 'Strongest Fighter in the West...',
+        stats: [
+          {
+            strength: 18,
+            dexterity: 12,
+            constitution: 14,
+            intelligence: 10,
+            wisdom: 14,
+            charisma: 13,
+            hitpoints: 56,
+            experience: 14000
+          }
+        ]
+      },
+      {
+        id: 2,
+        name: 'Edward Hamilton',
+        race: 'Human',
+        heroClass: 'Bard',
+        bio: 'An ambitious entreprenuer',
+        stats: [
+          {
+            strength: 12,
+            dexterity: 13,
+            constitution: 11,
+            intelligence: 16,
+            wisdom: 13,
+            charisma: 18,
+            hitpoints: 40,
+            experience: 13900
+          }
+        ]
+      }
+    ]
+  };
+
+  const [state, dispatch] = useReducer(characterReducer, initialState);
+
+  // Add Character
+
+  // Delete Character
+
+  // Set Current Character
+
+  // Clear Current Character
+
+  // Update Character
+
+  // Filter Characters
+
+  // Clear Filter
+
+  return (
+    <CharacterContext.Provider
+      value={{
+        characters: state.characters
+      }}
+    >
+      {props.children}
+    </CharacterContext.Provider>
+  );
+};
+
+export default CharacterState;
