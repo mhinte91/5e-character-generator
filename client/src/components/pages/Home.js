7086b7070e4ce90e6421ed10a3380e7891b02857
Michael Hinte <mhinte91@gmail.com>
2019-11-25 13:47:31 -0600

    loads user and sets token

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Characters from '../characters/Characters';
 import CharacterForm from '../characters/CharacterForm';
 import CharacterFilter from '../characters/CharacterFilter';
+import AuthContext from '../../context/auth/authContext';
 
 const Home = () => {
+  const authContext = useContext(AuthContext);
+
+  useEffect(() => {
+    authContext.loadUser();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div className='grid-2'>
       <div>
