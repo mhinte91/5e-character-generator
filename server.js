709c746adb652e9e351d9e794b432ae56a7b20a9
Michael Hinte <mhinte91@gmail.com>
2019-11-25 22:57:53 -0600

    removed initial route

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ connectDB();
 // Initiate Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the Character Generator API' })
-);
-
 // Define our routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
